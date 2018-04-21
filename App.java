3517f3be070a9070e4128d2fff371ee111b7aa64
z3nd0g <bogdan.krol@gmail.com>
2018-04-21 19:30:55 +0200

    second commit

--- App.java
+++ App.java
@@ -8,6 +8,7 @@ import javafx.scene.text.*;
 import javafx.scene.layout.*;
 import java.sql.*;
 import java.io.*;
+import javafx.collections.FXCollections;
 
 public class App extends Application {
 
@@ -16,7 +17,9 @@ public class App extends Application {
 
         Connector c = new Connector();
         Query q = new Query();
-        Picture pic = new Picture();       
+        String transaction1 = "INSERT INTO PICTURE VALUES (16, 13, 'ENGLISH', 'TERMINATOR 5', 2018, 1190515681, 200000000, TRUE, 3);";
+        String transaction2 = "INSERT INTO STARS_IN VALUES (11, 1666, 'Sarah');";     
+        String Transaction_query = "SELECT PERSON.PERSON_ID, PERSON.FIRST_NAME, PERSON.LAST_NAME, STARS_IN.CHARACTER, PICTURE.TITLE FROM PERSON, STARS_IN, PICTURE WHERE PERSON.PERSON_ID = STARS_IN.ACTOR_PERSON_PERSON_ID AND STARS_IN.PICTURE_PICTURE_ID = PICTURE.PICTURE_ID";   
     
         Text t1 = new Text(10, 20, "Show table: ");  
         t1.setFont(Font.font ("Verdana", 16));
@@ -30,53 +33,156 @@ public class App extends Application {
         Text t4 = new Text(10, 340, "Result: ");  
         t4.setFont(Font.font ("Verdana", 16));
 
-        Button btn_2 = new Button("PICTURE");
-        btn_2.setLayoutX(10);
-        btn_2.setLayoutY(40);        
-
-        Button btn_1 = new Button("PERSON");
-        btn_1.setLayoutX(10);
-        btn_1.setLayoutY(80);
-     
-        Button btn_4 = new Button("DIRECTOR");
-        btn_4.setLayoutX(10);
-        btn_4.setLayoutY(120);
-
-        Button btn_5 = new Button("AWARD");
-        btn_5.setLayoutX(10);
-        btn_5.setLayoutY(160);
-
-        Button btn_3 = new Button("Clear Text Area");
-        btn_3.setLayoutX(10);
-        btn_3.setLayoutY(660);
-
         TextArea textArea = new TextArea();
         textArea.setLayoutX(10);
         textArea.setLayoutY(360);        
         textArea.setPrefHeight(280);
         textArea.setPrefWidth(800); 
 
-        btn_1.setOnAction(new EventHandler<ActionEvent>() {
+        TextArea trans1 = new TextArea(transaction1);
+        trans1.setWrapText(true);
+        trans1.setLayoutX(850);
+        trans1.setLayoutY(360);        
+        trans1.setPrefHeight(40);
+        trans1.setPrefWidth(500);
+
+        TextArea trans2 = new TextArea(transaction2);
+        trans2.setWrapText(true);
+        trans2.setLayoutX(850);
+        trans2.setLayoutY(420);        
+        trans2.setPrefHeight(40);
+        trans2.setPrefWidth(500);
+
+        TextArea textArea_query = new TextArea(Transaction_query);
+        textArea_query.setWrapText(true);
+        textArea_query.setLayoutX(850);
+        textArea_query.setLayoutY(520);        
+        textArea_query.setPrefHeight(120);
+        textArea_query.setPrefWidth(500);
+
+        Button btn_picture = new Button("PICTURE");
+        btn_picture.setLayoutX(10);
+        btn_picture.setLayoutY(40);        
+
+        btn_picture.setOnAction(new EventHandler<ActionEvent>() {
+          public void handle(ActionEvent event) {
+            //q.show_PICTURE(c.conn);
+            String sql = "SELECT * FROM PICTURE";
+            q.execute_query(c.conn, sql);
+            textArea.setText(q.text_file);         
+            }         
+        });
+
+        Button btn_person = new Button("PERSON");
+        btn_person.setLayoutX(10);
+        btn_person.setLayoutY(80);
+
+        btn_person.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
-                q.show_PERSON(c.conn); 
+                //q.show_PERSON(c.conn);
+                String sql = "SELECT * FROM PERSON";
+                q.execute_query(c.conn, sql);
+                textArea.setText(q.text_file);
+            }
+        });
+     
+          
+
+        Button btn_award = new Button("AWARD");
+        btn_award.setLayoutX(10);
+        btn_award.setLayoutY(160);
+
+        btn_award.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                String sql = "SELECT * FROM AWARD";
+                q.execute_query(c.conn, sql);
+                textArea.setText(q.text_file); 
+            }
+        });
+
+        Button btn_director = new Button("DIRECTOR");
+        btn_director.setLayoutX(10);
+        btn_director.setLayoutY(120);
+
+        btn_director.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                //q.show_PERSON(c.conn);
+                String sql = "SELECT * FROM PERSON, DIRECTOR WHERE PERSON_ID = PERSON_PERSON_ID";
+                q.execute_query(c.conn, sql);
                 textArea.setText(q.text_file);
             }
         });
 
-        btn_2.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                q.show_PICTURE(c.conn);
-                textArea.setText(q.text_file);         
-            }
-                
-        });
+        Button btn_clear = new Button("Clear Text Area");
+        btn_clear.setLayoutX(10);
+        btn_clear.setLayoutY(660);
 
-        btn_3.setOnAction(new EventHandler<ActionEvent>() {
+
+     
+        btn_clear.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 textArea.clear();
             }
         });
+
+        Button btn_transaction = new Button("Transaction");
+        btn_transaction.setLayoutX(850);
+        btn_transaction.setLayoutY(480);
+
+       btn_transaction.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                try{
+                   c.conn.setAutoCommit(false);
+                   Statement stmt = c.conn.createStatement();
+
+                   String sql = trans1.getText();
+                   stmt.executeUpdate(sql);  
+
+                   sql = trans2.getText();
+                   stmt.executeUpdate(sql);               
+                   // no error.
+                   c.conn.commit();
+                   textArea.setText("Transaction executed."); 
+                   stmt.close();                   
+                   c.conn.setAutoCommit(true);
+                }catch(SQLException se){
+                   // error
+                   try{
+                      c.conn.rollback();
+                      textArea.setText("Transaction rolled back");
+                   }
+                   catch(SQLException se2){
+                }
+                }
+
+            }
+        });
+
+        Button btn_query = new Button("Execute Query");
+        btn_query.setLayoutX(850);
+        btn_query.setLayoutY(660);
+
+
+        btn_query.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                String text_field_query = textArea_query.getText();
+                q.execute_query(c.conn, text_field_query);
+                //textArea.setText("Query executed."); 
+                textArea.setText(q.text_file);
+            }
+        });
       
+
+        ChoiceBox cb1 = new ChoiceBox(FXCollections.observableArrayList("SELECT", "INSERT INTO"));
+        ChoiceBox cb2 = new ChoiceBox(FXCollections.observableArrayList("ALL", "FIRST_NAME", "LAST_NAME"));
+        ChoiceBox cb3 = new ChoiceBox(FXCollections.observableArrayList("FROM"));
+        ChoiceBox cb4 = new ChoiceBox(FXCollections.observableArrayList("PICTURE", "PERSON", "AWARD"));
+        ChoiceBox cb6 = new ChoiceBox(FXCollections.observableArrayList("PICTURE", "PERSON", "AWARD"));
+        ChoiceBox cb7 = new ChoiceBox(FXCollections.observableArrayList("WHERE"));
+        ChoiceBox cb8 = new ChoiceBox(FXCollections.observableArrayList("PERSON_ID", "PERSON_PERSON_ID"));
+        ChoiceBox cb9 = new ChoiceBox(FXCollections.observableArrayList("=", ">", "<"));
+        ChoiceBox cb10 = new ChoiceBox(FXCollections.observableArrayList("PERSON_ID", "PERSON_PERSON_ID"));
+
         TextField picture_id = new TextField ();
             picture_id.setPromptText("PICTURE_ID");
             picture_id.setPrefColumnCount(6);
@@ -104,12 +210,30 @@ public class App extends Application {
         TextField director_id = new TextField ();
             director_id.setPromptText("DIRECTOR_ID");
             director_id.setPrefColumnCount(7);
+        
         Button add = new Button("Add");
+
+       add.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {                       
+                String query_add = "INSERT INTO PICTURE VALUES(" + picture_id.getText() + ", " + rating.getText() + ", '" + language.getText() + "', '" + title.getText() + "', " + year.getText() + ", " + gross.getText() + ", " + budget.getText() + ", " + color.getText() + ", " + director_id.getText()+")";
+            
+                q.execute_update(c.conn, query_add);        
+            }
+                
+        });
 
         TextField pic_id = new TextField ();
             pic_id.setPromptText("PICTURE_ID");
             pic_id.setPrefColumnCount(6);
+        
         Button delete = new Button("Delete");
+
+        delete.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                String query_delete = "DELETE FROM PICTURE WHERE PICTURE_ID = " + pic_id.getText();
+                q.execute_update(c.conn, query_delete);
+            }
+        }); 
 
         HBox hb = new HBox();
         hb.getChildren().addAll(picture_id, rating, language, title, year, gross, budget, color, director_id, add, pic_id, delete);
@@ -117,34 +241,16 @@ public class App extends Application {
         hb.setLayoutX(150);
         hb.setLayoutY(40);
 
-        add.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                       
-                String query_add = "INSERT INTO PICTURE VALUES(" + picture_id.getText() + ", " + rating.getText() + ", '" + language.getText() + "', '" + title.getText() + "', " + year.getText() + ", " + gross.getText() + ", " + budget.getText() + ", " + color.getText() + ", " + director_id.getText()+")";
-            
-                q.execute_update(c.conn, query_add);
-                /*pic.setID(Integer.parseInt(picture_id.getText()));
-                pic.setPGRating(Integer.parseInt(rating.getText()));
-                pic.setLanguage(language.getText());
-                pic.setTitle(title.getText());
-                pic.setProductionYear(Integer.parseInt(year.getText()));
-                pic.setWorldwideGross(Integer.parseInt(gross.getText()));
-                ...
-                */        
-            }
-                
-        });
-
-        delete.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                String query_delete = "DELETE FROM PICTURE WHERE PICTURE_ID = " + pic_id.getText();
-                q.execute_update(c.conn, query_delete);
-            }
-        });
+        HBox hb2 = new HBox();
+        hb2.getChildren().addAll(cb1, cb2, cb3, cb4, cb6, cb7, cb8, cb9, cb10);
+        hb2.setSpacing(10);
+        hb2.setLayoutX(10);
+        hb2.setLayoutY(280);
+     
 
         stage.setTitle("DB");
         Group root = new Group();
-        root.getChildren().addAll(t1, t2, t3, t4, btn_2, btn_1, btn_3, btn_4, btn_5, textArea, hb);
+        root.getChildren().addAll(t1, t2, t3, t4, btn_picture, btn_person, btn_clear, btn_director, btn_award, btn_transaction, btn_query, textArea, trans1, trans2, textArea_query, hb, hb2);
 
         Scene scene = new Scene(root, 1350, 700);
         scene.setFill(Color.GREY);
--- Query.java
+++ Query.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class Query {
 
     public String text_file = "";
-
+    public int number_of_columns = 0;     
     
     public void execute_update(Connection c, String query) {
         try {
@@ -15,28 +15,61 @@ public class Query {
 
             s.close();
 
-            } catch (SQLException e) {
+        }catch (SQLException e) {
                 e.printStackTrace();
-            }catch (Exception e) {
+        }catch (Exception e) {
                 e.printStackTrace();
-            }
-
+        }
     }
-
     
     public void execute_query(Connection c, String query) {
         try {
             Statement s = c.createStatement();
             ResultSet rs = s.executeQuery(query);
             System.out.println("*************************query********************************");
+            
+            ResultSetMetaData rsmd = rs.getMetaData();
+            number_of_columns = rsmd.getColumnCount();
 
+            FileWriter filewriter = new FileWriter("File3.txt");
+            BufferedWriter writer = new BufferedWriter(filewriter);
+
+            while (rs.next()) {         
+                for(int i = 1 ; i <= number_of_columns; i++){
+                    writer.write(rs.getString(i) + ", ");
+                      //System.out.print(rs.getString(i) + " ");
+                }
+                writer.newLine();
+                  //System.out.println();
+            }
+
+            writer.close();
+
+            BufferedReader br = new BufferedReader(new FileReader("File3.txt"));
+
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                text_file = sb.toString();
+
+            //Clean-up environment
+            br.close();
             rs.close();
             s.close();
 
-            } catch (SQLException e) {
+        }catch (SQLException e) {
                 e.printStackTrace();
-            }
+        }catch(IOException e) {
+                e.printStackTrace();
+        } 
     }
+
+/*
 
     public void show_PICTURE(Connection c) {
         try {
@@ -86,7 +119,7 @@ public class Query {
     }
 
 
-public void show_PERSON(Connection c) {
+    public void show_PERSON(Connection c) {
         try {
             Statement s = c.createStatement();
             ResultSet rs = s.executeQuery("SELECT * FROM PERSON;");
@@ -100,8 +133,11 @@ public void show_PERSON(Connection c) {
                writer.write(Integer.toString(rs.getInt("PERSON_ID")) + ", ");
                writer.write(rs.getString("FIRST_NAME") + ", ");
                writer.write(rs.getString("LAST_NAME") + ", ");
+               writer.write(rs.getString("GENDER") + ", ");
+               writer.write(rs.getString("EMAIL") + ", ");
+               writer.write(rs.getString("BIRTH_DATE") + ", ");
+               writer.write(rs.getString("BIRTH_PLACE") + ", ");
                writer.newLine();
- 
             }
             writer.close();
 
@@ -128,5 +164,47 @@ public void show_PERSON(Connection c) {
                 e.printStackTrace();
             } 
     }
+
+    public void show_AWARD(Connection c) {
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery("SELECT * FROM PERSON;");
+            System.out.println("*************************query********************************");
+
+            FileWriter filewriter = new FileWriter("File3.txt");
+            BufferedWriter writer = new BufferedWriter(filewriter);
+
+            while(rs.next()){
+               //Retrieve by column name
+               writer.write(Integer.toString(rs.getInt("AWARD_ID")) + ", ");
+               //writer.write(rs.getString("AWARD_ID") + ", ");
+               writer.write(rs.getString("NAME") + ", ");
+               writer.newLine();
+            }
+            writer.close();
+
+            BufferedReader br = new BufferedReader(new FileReader("File3.txt"));
+
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                text_file = sb.toString();
+
+            //Clean-up environment
+            br.close();
+            rs.close();
+            s.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch(IOException e) {
+                e.printStackTrace();
+            } 
+    } */
 }
 
